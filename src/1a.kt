9f4dfc0569fe108e44f1e9a52b95571e3709c4f2
Masseeha <masseeha7@gmail.com>
2026-06-01 19:21:00 +0530

    tried displaying values using + symbol but it ended up adding all the values

--- src/1a.kt
+++ src/1a.kt
@@ -1,14 +1,14 @@
 import kotlin.math.pow
 
 fun main(){
-    print("Enter num1")
+    println("Enter num1 ")
     val n1 = readln().toInt()
-    print("Enter num2")
+    println("Enter num2 ")
     val n2 = readln().toInt()
     val sum1 = n1+n2
     val dif1 = n1-n2
     val prd1 = n1*n2
-    val div1 = n1.toDouble() / n2
+    val div1 = n1.toDouble()/n2.toDouble()
     val div2 = n1/n2
     val rem1 = n1 % n2
     val exp1 = n1.toDouble().pow(n2.toDouble())
---
+++ src/1b.kt
@@ -0,0 +1,21 @@
+import kotlin.math.pow
+
+fun calc1(n1:Int, n2:Int){
+    val sum1 = n1+n2
+    val dif1 = n1-n2
+    val prd1 = n1*n2
+    val div1 = n1.toDouble()/n2.toDouble()
+    val div2 = n1/n2
+    val rem1 = n1%n2
+    val exp1 = n1.toDouble().pow(n2.toDouble())
+
+    println("$sum1, $dif1, $prd1,  $div1, $div2, $rem1, $exp1")
+}
+fun main(){
+    println("Enter num1: ")
+    val n1 = readln().toInt()
+    println("Enter num2: ")
+    val n2 = readln().toInt()
+
+    calc1(n1,n2)
+}
