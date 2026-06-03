019b9a7af7b4344ace6c71a98086594f10e4ec66
Masseeha <masseeha7@gmail.com>
2026-06-03 14:19:32 +0530

    padStart() with leading zeros applied to minute variable

--- src/3a.kt
+++ src/3a.kt
@@ -6,16 +6,19 @@ fun main(){
     var hourAngle = 0.0
     var angle = 0.0
 
+
     for(hour in 9..20){
         for(minute in 0..55 step 5) {
             minuteAngle = minute * 6.0
-            hourAngle = (hour % 12) * 30 + minute * 0.5
+            hourAngle = (hour % 12) * 30 + (minute * 0.5)
             angle = abs(hourAngle - minuteAngle)
             if (angle > 180) {
                 angle = 360 - angle
             }
-            println("${hour%12}:$minute: $angle degrees")
+            var minute1= minute.toString().padStart(2,'0')
+            println("${hour%12}:$minute1 - $angle degrees")
         }
+        println()
     }
 }
 
