37e132df00ab56d27440e0fb5d5c442e952d6737
Masseeha <masseeha7@gmail.com>
2026-06-04 22:59:49 +0530

    questions are shuffled without repetition but answers arent aligned w them

---
+++ src/8b.kt
@@ -0,0 +1,53 @@
+import java.io.File
+
+fun main() {
+    val lines = File("gk1.txt").readLines()
+    val countries = mutableListOf<String>()
+    val capitals = mutableListOf<String>()
+    val answer = mutableListOf<String>()
+    val score  = mutableListOf<Double>()
+    var question = ""
+    for (i in 0..9) {
+        val parts = lines[i].trim().split(",")
+        var part1 = parts[0]
+        var part2 = parts[1]
+        countries.add(part1)
+        capitals.add(part2)
+    }
+
+    countries.shuffle()
+    for (i in 0 until 9) {
+        println("What is the capital of " + "${countries[i]}")
+        val answer1 = readln()
+        answer.add(answer1)
+    }
+    println()
+
+    for (i in 0 until 9) {
+        if (answer[i].trim().lowercase() == capitals[i].trim().lowercase()) {
+            score.add(10.00)
+        } else {
+            score.add(0.00)
+        }
+    }
+
+    println()
+
+    val total = score.sum()
+    println("Total marks: "+ total)
+    println("Wrongly Answered Questions:")
+    for (i in 0 until 9) {
+        if (score[i]== 0.00) {
+            println("What is the capital of "  +countries[i]+ "?")
+            println("Correct answer: "+ capitals[i])
+        }
+    }
+}
+
+
+
+
+
+
+
+
